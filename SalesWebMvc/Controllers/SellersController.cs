2ad4c7cd0573695f731ff7c7897b89dcf2d6d6da
TiagoFRodrigues <tfr_ch@hotmail.com>
2020-05-14 18:10:12 -0300

    Simple create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -24,5 +24,19 @@ namespace SalesWebMvc.Controllers
             List<Seller> sellers = _sellerService.FindAll();
             return View(sellers);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
